35bdc4e2d6a25e27fdced53cb2881288cf0f6f53
Anonymous Maarten <anonymous.maarten@gmail.com>
2019-05-25 02:43:27 +0200

    just run glslangValidator

--- test_package_installer/conanfile.py
+++ test_package_installer/conanfile.py
@@ -7,6 +7,4 @@ class VulkanTestConan(ConanFile):
     exports_sources = ['shader.frag', ]
 
     def test(self):
-        if not tools.cross_building(self.settings):
-            with tools.environment_append(RunEnvironment(self).vars):
-                self.run('glslangValidator -V "{}/shader.frag"'.format(self.source_folder))
+        self.run('glslangValidator -V "{}/shader.frag"'.format(self.source_folder))
